1aaa21753b6f46bbdc1c5baca523d2242d844711
Charles Perier <charles.perier@student-cs.fr>
2022-09-20 14:35:19 +0200

    create connection with mysql database and add service entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'panorama',
+      password: 'panorama',
+      database: 'panorama-db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // shouldn't be used in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
