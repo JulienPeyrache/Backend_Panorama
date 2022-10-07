f9ad5b2c478460d174c581284bb9ffacc6bb95f4
Charles Perier <charles.perier@student-cs.fr>
2022-10-07 16:18:44 +0200

    add route attached-service/findByServiceAndLabel

--- src/attached_service/attached_service.controller.ts
+++ src/attached_service/attached_service.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
 } from "@nestjs/common";
 import { AttachedServiceService } from "./attached_service.service";
 import { CreateAttachedServiceDto } from "./dto/create-attached_service.dto";
@@ -15,50 +15,58 @@ import { ApiTags } from "@nestjs/swagger";
 @ApiTags("attached-service")
 @Controller("attached-service")
 export class AttachedServiceController {
-  constructor(
-    private readonly attachedServiceService: AttachedServiceService
-  ) {}
+	constructor(
+		private readonly attachedServiceService: AttachedServiceService
+	) {}
 
-  @Post()
-  create(@Body() createAttachedServiceDto: CreateAttachedServiceDto) {
-    return this.attachedServiceService.create(createAttachedServiceDto);
-  }
+	@Post()
+	create(@Body() createAttachedServiceDto: CreateAttachedServiceDto) {
+		return this.attachedServiceService.create(createAttachedServiceDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.attachedServiceService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.attachedServiceService.findAll();
+	}
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.attachedServiceService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.attachedServiceService.findOne(+id);
+	}
 
-  @Patch(":id")
-  update(
-    @Param("id") id: string,
-    @Body() updateAttachedServiceDto: UpdateAttachedServiceDto
-  ) {
-    return this.attachedServiceService.update(+id, updateAttachedServiceDto);
-  }
+	@Get("findByServiceAndLabel/:serviceId/:label")
+	findByServiceAndLabel(
+		@Param("serviceId") serviceId: string,
+		@Param("label") label: string
+	) {
+		return this.attachedServiceService.findByServiceAndLabel(+serviceId, label);
+	}
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.attachedServiceService.remove(+id);
-  }
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body() updateAttachedServiceDto: UpdateAttachedServiceDto
+	) {
+		return this.attachedServiceService.update(+id, updateAttachedServiceDto);
+	}
 
-  @Patch(":id/addToSite/:siteId")
-  addToSite(@Param("id") id: string, @Param("siteId") siteId: string) {
-    return this.attachedServiceService.addToSite(+id, +siteId);
-  }
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.attachedServiceService.remove(+id);
+	}
 
-  @Patch(":id/removeFromSite/:siteId")
-  removeFromSite(@Param("id") id: string, @Param("siteId") siteId: string) {
-    return this.attachedServiceService.removeFromSite(+id, +siteId);
-  }
+	@Patch(":id/addToSite/:siteId")
+	addToSite(@Param("id") id: string, @Param("siteId") siteId: string) {
+		return this.attachedServiceService.addToSite(+id, +siteId);
+	}
 
-  @Get(":id/isOnSite/:siteId")
-  isOnSite(@Param("id") id: string, @Param("siteId") siteId: string) {
-    return this.attachedServiceService.isOnSite(+id, +siteId);
-  }
+	@Patch(":id/removeFromSite/:siteId")
+	removeFromSite(@Param("id") id: string, @Param("siteId") siteId: string) {
+		return this.attachedServiceService.removeFromSite(+id, +siteId);
+	}
+
+	@Get(":id/isOnSite/:siteId")
+	isOnSite(@Param("id") id: string, @Param("siteId") siteId: string) {
+		return this.attachedServiceService.isOnSite(+id, +siteId);
+	}
 }
--- src/attached_service/attached_service.service.ts
+++ src/attached_service/attached_service.service.ts
@@ -8,87 +8,99 @@ import { AttachedService } from "./entities/attached_service.entity";
 
 @Injectable()
 export class AttachedServiceService {
-  constructor(
-    @InjectRepository(AttachedService)
-    private readonly attachedServiceRepository: Repository<AttachedService>,
-    @InjectRepository(Site)
-    private readonly siteRepository: Repository<Site>
-  ) {}
+	constructor(
+		@InjectRepository(AttachedService)
+		private readonly attachedServiceRepository: Repository<AttachedService>,
+		@InjectRepository(Site)
+		private readonly siteRepository: Repository<Site>
+	) {}
 
-  async create(
-    createAttachedServiceDto: CreateAttachedServiceDto
-  ): Promise<AttachedService> {
-    return this.attachedServiceRepository.save(createAttachedServiceDto);
-  }
+	async create(
+		createAttachedServiceDto: CreateAttachedServiceDto
+	): Promise<AttachedService> {
+		return this.attachedServiceRepository.save(createAttachedServiceDto);
+	}
 
-  async findAll(): Promise<AttachedService[]> {
-    return this.attachedServiceRepository.find({ relations: ["sites"] });
-  }
+	async findAll(): Promise<AttachedService[]> {
+		return this.attachedServiceRepository.find({ relations: ["sites"] });
+	}
 
-  async findOne(id: number): Promise<AttachedService> {
-    return this.attachedServiceRepository.findOne({
-      where: {
-        id: id,
-      },
-      relations: ["sites"],
-    });
-  }
+	async findOne(id: number): Promise<AttachedService> {
+		return this.attachedServiceRepository.findOne({
+			where: {
+				id: id,
+			},
+			relations: ["sites"],
+		});
+	}
 
-  async update(
-    id: number,
-    updateAttachedServiceDto: UpdateAttachedServiceDto
-  ): Promise<UpdateResult> {
-    return this.attachedServiceRepository.update(id, updateAttachedServiceDto);
-  }
+	async findByServiceAndLabel(
+		serviceId: number,
+		label: string
+	): Promise<AttachedService[]> {
+		return this.attachedServiceRepository.find({
+			where: {
+				serviceId: serviceId,
+				label_attached_service: label,
+			},
+		});
+	}
 
-  async remove(id: number): Promise<DeleteResult> {
-    return this.attachedServiceRepository.delete(id);
-  }
+	async update(
+		id: number,
+		updateAttachedServiceDto: UpdateAttachedServiceDto
+	): Promise<UpdateResult> {
+		return this.attachedServiceRepository.update(id, updateAttachedServiceDto);
+	}
 
-  async isOnSite(attachedServiceId: number, siteId: number): Promise<Site> {
-    const attachedService = await this.attachedServiceRepository.findOne({
-      where: {
-        id: attachedServiceId,
-      },
-      relations: ["sites"],
-    });
-    for (const site of attachedService.sites) {
-      if (site.id === siteId) {
-        return site;
-      }
-    }
-    throw new NotFoundException();
-  }
+	async remove(id: number): Promise<DeleteResult> {
+		return this.attachedServiceRepository.delete(id);
+	}
 
-  async addToSite(
-    attachedServiceId: number,
-    siteId: number
-  ): Promise<AttachedService> {
-    const attachedService = await this.attachedServiceRepository.findOneBy({
-      id: attachedServiceId,
-    });
-    const site = await this.siteRepository.findOneBy({ id: siteId });
-    if (attachedService.sites === undefined) {
-      attachedService.sites = [site];
-    } else {
-      attachedService.sites.push(site);
-    }
-    return this.attachedServiceRepository.save(attachedService);
-  }
+	async isOnSite(attachedServiceId: number, siteId: number): Promise<Site> {
+		const attachedService = await this.attachedServiceRepository.findOne({
+			where: {
+				id: attachedServiceId,
+			},
+			relations: ["sites"],
+		});
+		for (const site of attachedService.sites) {
+			if (site.id === siteId) {
+				return site;
+			}
+		}
+		throw new NotFoundException();
+	}
 
-  async removeFromSite(
-    attachedServiceId: number,
-    siteId: number
-  ): Promise<AttachedService> {
-    const attachedService = await this.attachedServiceRepository.findOne({
-      where: {
-        id: attachedServiceId,
-      },
-      relations: ["sites"],
-    });
-    attachedService.sites = attachedService.sites.filter(
-      (site) => site.id !== siteId
-    );
-    return this.attachedServiceRepository.save(attachedService);
-  }
+	async addToSite(
+		attachedServiceId: number,
+		siteId: number
+	): Promise<AttachedService> {
+		const attachedService = await this.attachedServiceRepository.findOneBy({
+			id: attachedServiceId,
+		});
+		const site = await this.siteRepository.findOneBy({ id: siteId });
+		if (attachedService.sites === undefined) {
+			attachedService.sites = [site];
+		} else {
+			attachedService.sites.push(site);
+		}
+		return this.attachedServiceRepository.save(attachedService);
+	}
+
+	async removeFromSite(
+		attachedServiceId: number,
+		siteId: number
+	): Promise<AttachedService> {
+		const attachedService = await this.attachedServiceRepository.findOne({
+			where: {
+				id: attachedServiceId,
+			},
+			relations: ["sites"],
+		});
+		attachedService.sites = attachedService.sites.filter(
+			(site) => site.id !== siteId
+		);
+		return this.attachedServiceRepository.save(attachedService);
+	}
 }
