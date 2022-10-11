ab062bf1c3a5893090700b74e5a5a93edca0402a
Charles Perier <charles.perier@student-cs.fr>
2022-10-11 18:18:47 +0200

    update updateAll

--- src/value_equipment_building/value_equipment_building.controller.ts
+++ src/value_equipment_building/value_equipment_building.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
 } from "@nestjs/common";
 import { ValueEquipmentBuildingService } from "./value_equipment_building.service";
 import { CreateValueEquipmentBuildingDto } from "./dto/create-value_equipment_building.dto";
@@ -15,59 +15,63 @@ import { ApiTags } from "@nestjs/swagger";
 @ApiTags("value-equipment-building")
 @Controller("value-equipment-building")
 export class ValueEquipmentBuildingController {
-  constructor(
-    private readonly valueEquipmentBuildingService: ValueEquipmentBuildingService
-  ) {}
+	constructor(
+		private readonly valueEquipmentBuildingService: ValueEquipmentBuildingService
+	) {}
 
-  @Post()
-  create(
-    @Body()
-    createValueEquipmentBuildingDto: CreateValueEquipmentBuildingDto
-  ) {
-    return this.valueEquipmentBuildingService.create(
-      createValueEquipmentBuildingDto
-    );
-  }
+	@Post()
+	create(
+		@Body()
+		createValueEquipmentBuildingDto: CreateValueEquipmentBuildingDto
+	) {
+		return this.valueEquipmentBuildingService.create(
+			createValueEquipmentBuildingDto
+		);
+	}
 
-  @Get()
-  findAll() {
-    return this.valueEquipmentBuildingService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.valueEquipmentBuildingService.findAll();
+	}
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.valueEquipmentBuildingService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.valueEquipmentBuildingService.findOne(+id);
+	}
 
-  @Get("/findByBuildingId/:buildingId")
-  findWithIds(@Param("buildingId") buildingId: string) {
-    return this.valueEquipmentBuildingService.findByBuildingId(+buildingId);
-  }
+	@Get("/findByBuildingId/:buildingId")
+	findWithIds(@Param("buildingId") buildingId: string) {
+		return this.valueEquipmentBuildingService.findByBuildingId(+buildingId);
+	}
 
-  @Patch(":id")
-  update(
-    @Param("id") id: string,
-    @Body()
-    updateValueEquipmentBuildingDto: UpdateValueEquipmentBuildingDto
-  ) {
-    return this.valueEquipmentBuildingService.update(
-      +id,
-      updateValueEquipmentBuildingDto
-    );
-  }
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body()
+		updateValueEquipmentBuildingDto: UpdateValueEquipmentBuildingDto
+	) {
+		return this.valueEquipmentBuildingService.update(
+			+id,
+			updateValueEquipmentBuildingDto
+		);
+	}
 
-  @Post("/updateAll")
-  updateAll(
-    @Body()
-    listValuesEquipmentBuilding: UpdateValueEquipmentBuildingDto[]
-  ) {
-    for (const value of listValuesEquipmentBuilding) {
-      this.valueEquipmentBuildingService.update(+value.id, value);
-    }
-  }
+	@Post("/updateAll")
+	updateAll(
+		@Body()
+		listValuesEquipmentBuilding: any[]
+	) {
+		for (const value of listValuesEquipmentBuilding) {
+			if (value.id) {
+				this.valueEquipmentBuildingService.update(+value.id, value);
+			} else {
+				this.valueEquipmentBuildingService.create(value);
+			}
+		}
+	}
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.valueEquipmentBuildingService.remove(+id);
-  }
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.valueEquipmentBuildingService.remove(+id);
+	}
 }
