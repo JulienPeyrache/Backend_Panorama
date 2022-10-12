75968690c2a08897a0e2fb0345a1861e2ba5b02d
Anna Grigoriu <anna-agatha.grigoriu@student-cs.fr>
2022-10-12 21:14:39 +0200

    docker implementation

--- src/database/typeormoptions.config.ts
+++ src/database/typeormoptions.config.ts
@@ -1,15 +1,15 @@
 import { DataSourceOptions } from "typeorm";
 
 const ormConfigOptions: DataSourceOptions = {
-	type: "mysql",
-	host: "localhost",
-	port: 3306,
-	username: "panorama",
-	password: "panorama",
-	database: "panorama-db",
-	entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-	synchronize: false, // shouldn't be true in production
-	migrations: [__dirname + "/**/migrations/*{.ts,.js}"],
+  type: "mysql",
+  host: process.env.DATABASE_HOST,
+  port: 3306,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+  synchronize: false, // shouldn't be true in production
+  migrations: [__dirname + "/**/migrations/*{.ts,.js}"],
 };
 
 export default ormConfigOptions;
