0de0ddaa718891640689354a8398e385377c63fe
Charles Perier <charles.perier@student-cs.fr>
2022-10-17 22:50:51 +0200

    add route to sort building by name

--- src/building/building.service.ts
+++ src/building/building.service.ts
@@ -7,35 +7,39 @@ import { Building } from "./entities/building.entity";
 
 @Injectable()
 export class BuildingService {
-  constructor(
-    @InjectRepository(Building)
-    private readonly buildingRepository: Repository<Building>
-  ) {}
+	constructor(
+		@InjectRepository(Building)
+		private readonly buildingRepository: Repository<Building>
+	) {}
 
-  async create(createBuildingDto: CreateBuildingDto): Promise<Building> {
-    return this.buildingRepository.save(createBuildingDto);
-  }
+	async create(createBuildingDto: CreateBuildingDto): Promise<Building> {
+		return this.buildingRepository.save(createBuildingDto);
+	}
 
-  async findAll(): Promise<Building[]> {
-    return this.buildingRepository.find();
-  }
+	async findAll(): Promise<Building[]> {
+		return this.buildingRepository.find();
+	}
 
-  async findOne(id: number): Promise<Building> {
-    return this.buildingRepository.findOneBy({ id: id });
-  }
+	async findAllSortedByName(): Promise<Building[]> {
+		return this.buildingRepository.find({ order: { name_building: "ASC" } });
+	}
 
-  async findByName(name: string): Promise<Building> {
-    return this.buildingRepository.findOneBy({ name_building: name });
-  }
+	async findOne(id: number): Promise<Building> {
+		return this.buildingRepository.findOneBy({ id: id });
+	}
 
-  async update(
-    id: number,
-    updateBuildingDto: UpdateBuildingDto
-  ): Promise<UpdateResult> {
-    return this.buildingRepository.update(id, updateBuildingDto);
-  }
+	async findByName(name: string): Promise<Building> {
+		return this.buildingRepository.findOneBy({ name_building: name });
+	}
 
-  async remove(id: number): Promise<DeleteResult> {
-    return this.buildingRepository.delete(id);
-  }
+	async update(
+		id: number,
+		updateBuildingDto: UpdateBuildingDto
+	): Promise<UpdateResult> {
+		return this.buildingRepository.update(id, updateBuildingDto);
+	}
+
+	async remove(id: number): Promise<DeleteResult> {
+		return this.buildingRepository.delete(id);
+	}
 }
