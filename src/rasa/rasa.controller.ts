465e710eb0b597441808fbb2272fb11cdf7f8499
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-10-24 11:46:32 +0200

    rasa controller

--- src/rasa/rasa.controller.ts
+++ src/rasa/rasa.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Param, Post } from "@nestjs/common";
+import {Step} from "../database/enum";
 
 @Controller("rasa")
 export class RasaController {
-    @Get()
-    findAll(): string {
-        return 'This action returns all cats';
-      }
+  @Get(':text')
+  findAll(@Param("text") text: string): Step[] {
+      let steps = [];
+      fetch("http://localhost:5005/model/parse", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: '{"text":"' + text + '"}',
+      })
+        .then((response) => response.json())
+        .then((response) => response.intent_ranking)
+        .then((response) => response.filter((intent) => (intent.confidence > 0.2)))
+        .then((response) => response.map((intent) => intent.name as Step))
+        .then((response) => {steps = response});
+        return steps;
+    }
     }
